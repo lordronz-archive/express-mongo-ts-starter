fe098cb1f82d535753a2878ed92e2e467675d607
LordRonz <aarontanhar2000@gmail.com>
2022-12-11 20:34:25 +0700

    test: initialize test :rocket:

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  testEnvironment: 'node',
+  testEnvironmentOptions: {
+    NODE_ENV: 'test',
+  },
+  restoreMocks: true,
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'src/config',
+    'src/app.js',
+    'tests',
+  ],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+};
+
+export default config;
---
+++ tests/fixtures/token.fixture.ts
@@ -0,0 +1,23 @@
+import moment from 'moment';
+
+import config from '../../src/config/config';
+import { tokenTypes } from '../../src/config/tokens';
+import tokenService from '../../src/services/token.service';
+import { admin, userOne } from './user.fixture';
+
+const accessTokenExpires = moment().add(
+  config.jwt.accessExpirationMinutes,
+  'minutes'
+);
+
+export const userOneAccessToken = tokenService.generateToken(
+  userOne._id.toString(),
+  accessTokenExpires,
+  tokenTypes.ACCESS
+);
+
+export const adminAccessToken = tokenService.generateToken(
+  admin._id.toString(),
+  accessTokenExpires,
+  tokenTypes.ACCESS
+);
